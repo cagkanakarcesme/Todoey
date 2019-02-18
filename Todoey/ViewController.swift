560c0984da9cdc043258ea09ccc7d041a6000017
Çağkan <cagkan@cagkan-macbook-pro.local>
2019-02-18 14:24:03 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Cagkan on 18.02.2019.
+//  Copyright © 2019 Cagkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
